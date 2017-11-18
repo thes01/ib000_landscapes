5ffcc8fd7da652f37d600876e78c33b74429cef9
Miroslav Šerý <athes01@gmail.com>
2017-11-18 23:47:09 +0100

    sine generator update

--- SineLandCutGenerator.py
+++ SineLandCutGenerator.py
@@ -5,10 +5,16 @@ from LandCutGenerator import *
 class SineLandCutGenerator(LandCutGenerator):
     discrete_sines = {} # a dictionary (cache) of key(number of steps) -> list(sine values)
 
-    def makeLandCut(self):
-        peaks = self.generatePeaks(10, 100)
+    def __init__(self, size, n_peaks: int, max_height: int):
+        self.n_peaks = n_peaks
+        self.max_height = max_height
 
-        step = ceil(self.size / len(peaks))
+        super().__init__(size)
+
+    def makeLandCut(self):
+        peaks = self.generatePeaks()
+
+        step = ceil(self.size / self.n_peaks)
 
         for peak_index in range(len(peaks) - 1):
             start_val = peaks[peak_index]
@@ -24,20 +30,24 @@ class SineLandCutGenerator(LandCutGenerator):
 
         print(self.arr)
 
-    def generatePeaks(self, n_peaks: int, max_height: int):
-        assert n_peaks < self.size
+    def generatePeaks(self):
+        assert self.n_peaks < self.size
 
-        step = ceil(self.size / n_peaks) # ceiling provides that the last point can be outside the area, must handle it in another function
+        step = ceil(self.size / self.n_peaks) # ceiling provides that the last point can be outside the area, must handle it in another function
 
         peaks = []
 
-        for i in range(n_peaks):
-            peaks.append(randint(0, max_height))
+        for i in range(self.n_peaks + 1):
+            peaks.append(randint(0, self.max_height))
 
         return peaks
 
     def removeRedundantPeaks(self):
         print("undefined")
+
+    # override max height function because we already know that from the settings
+    def getMaxHeight(self):
+        return self.max_height
 
     def getDiscreteSineValues(self, n: int, start_y: int, end_y: int):
         # compute the sine function from pi/2 to 3pi/2 and normalize 
