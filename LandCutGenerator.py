7f3370193fc115a3a7d5f283dc190a1c5ccb060b
Miroslav Šerý <athes01@gmail.com>
2017-11-18 22:10:51 +0100

    max height auto computing

--- LandCutGenerator.py
+++ LandCutGenerator.py
@@ -1,12 +1,11 @@
 import numpy as np
-from math import sqrt, cos, pi
+from math import sqrt, cos, pi, ceil
 from PIL import Image, ImageColor, ImageDraw
 
 class LandCutGenerator():
     def __init__(self, size):
         self.arr = np.zeros(size)
         self.size = size
-        self.max_height = 0
 
     def makeLandCut(self):
         raise NotImplementedError()
@@ -16,13 +15,23 @@ class LandCutGenerator():
 
         return True
 
+    def getMaxHeight(self):
+        __max = self.arr[0]
+
+        for i in range(len(self.arr)):
+            if self.arr[i] > __max:
+                __max = self.arr[i]
+
+        return int(ceil(__max))
+
     def generateImage(self):    
-        img = Image.new('RGB', (self.size, self.max_height))
+        max_height = self.getMaxHeight()
+    
+        img = Image.new('RGB', (self.size, max_height))
         draw = ImageDraw.Draw(img)
 
         for index in range(self.size):
             _height = self.arr[index]
-
-            draw.line((index, self.max_height, index, self.max_height -_height), fill=(0,255,10))
-        
+            draw.line((index, max_height, index, max_height -_height), fill=(0, 255, 10))   
+            
         return img
