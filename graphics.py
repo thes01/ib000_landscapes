a93744ec585d082f31ed586e7c4408307339a655
Miroslav Šerý <athes01@gmail.com>
2017-11-18 23:47:24 +0100

    testing

--- graphics.py
+++ graphics.py
@@ -1,8 +1,24 @@
-from RandomLandCutGenerator import *
+from SineLandCutGenerator import *
+from CombineLandGenerator import *
 
-gen = RandomLandCutGenerator(200)
-gen.applySmoothing(5)
+# gen = RandomLandCutGenerator(200)
+# gen.applySmoothing(5)
 
-gen.makeLandCut()
+# gen.makeLandCut()
 
-gen.generateImage().show()
+# gen.generateImage().show()
+
+landcut_horizontal = SineLandCutGenerator(300, 5, 100)
+landcut_vertical = SineLandCutGenerator(300, 10, 100)
+landcut_weights = SineLandCutGenerator(200, 5, 200)
+
+landcut_horizontal.makeLandCut()
+landcut_vertical.makeLandCut()
+landcut_weights.makeLandCut()
+
+# # landcut_vertical.generateImage().show()
+
+final = CombineLandGenerator(landcut_horizontal, landcut_vertical, landcut_weights)
+final.makeLand()
+
+final.generateImage().show()
